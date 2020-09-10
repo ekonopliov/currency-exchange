fb33ef2fb2984d5fe335bc0a7cffc30ed46c08d3
Konopliovas Ernestas <ernestas.konopliovas@mif.stud.vu.lt>
2020-09-10 09:39:50 +0300

    init commit

---
+++ src/test/java/lt/kono/currencyexchange/CurrencyExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.kono.currencyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
